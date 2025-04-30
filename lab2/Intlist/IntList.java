c49c7278a6efc6943798960624bdaea5b61791a3
XinAura <3317192540@qq.com>
2025-04-30 13:56:45 +0800

    completed lab2 fina

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,6 +81,7 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+
         if(A == null) return B;
         IntList intList = A;
         while(A.rest != null) {
